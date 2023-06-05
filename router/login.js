68734dc53307a2b15b60ce2b41dffbcd7726c664
yacine <lentente1958@gmail.com>
2023-06-05 16:03:50 +0100

    define action patrhhth

--- router/login.js
+++ router/login.js
@@ -1,6 +1,6 @@
-module.exports=(req,res)=>{  
-    res.render('../views/login.ejs',{
-        redirectParam:req.query.redirect ?? "",
+module.exports=(req,res)=>{
+    res.render('../views/login.ejs',{ 
+        path:`/auth${ req.query.redirect ? '?redirect='+req.query.redirect : ''}`,
         message:req.flash('error')
     });
 }
