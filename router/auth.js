b324f5fcd5d7ac0f862d8fda9d5e5164178e97d1
yacine <lentente1958@gmail.com>
2023-06-05 13:58:04 +0100

    splice auth into 2 methods

--- router/auth.js
+++ router/auth.js
@@ -1,3 +1,28 @@
-module.exports=(req,res)=>{
-  require('passport').authenticate('local',{failureRedirect:'/login'})
+async function auth(req,res,next){
+    const {passport}=require('../passport/index.js');
+    const checkCredentials=(req)=>{
+       const {email,password}=req.method =='POST' ? req.body : req.query;
+       console.log(req.query,req.body ,">>>>>>>>>>")
+       if(!email) return "Please, provide your email";
+       if(!password) return  "Please, provide your password";
+       return null;
+    }
+    
+      passport.authenticate('local', { 
+                  failureRedirect:'/login', 
+                  failureFlash:true,
+                  badRequestMessage:checkCredentials(req),
+                  failureMessage:true,
+      })(req,res,next);  
+         
 }
+function direction(req,res){
+  console.log(req.session)
+  const direction =req.query.redirect ?? "/";
+  res.redirect(direction);
+}
+
+module.exports={direction,auth} 
+  
+  // require('passport').authenticate('local',{failureRedirect:'/login'})
+ 
