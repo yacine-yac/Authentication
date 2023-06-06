0106c82ba81389dd3d8e82c97c1de8e8382e6919
yacine <lentente1958@gmail.com>
2023-06-06 10:16:55 +0100

    location redirect method after authentication

---
+++ static/js/handlers/location.js
@@ -0,0 +1,14 @@
+export default function location(){
+    const redirection=searchUrl("redirect");
+    window.location.href= redirection ?? "/"; 
+}
+/**
+ * Check if query params exists or no
+ * @param {string} parameter value looking for 
+ * @returns {null | string} 
+ */
+function searchUrl(parameter){
+    const query=window.location.search;
+    const usp= new URLSearchParams(query);
+    return usp.get(parameter) ;
+}
