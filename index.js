8b7d59821197da85c36d11fd2f1bf7073d6984e3
yacine <lentente1958@gmail.com>
2023-06-06 09:06:14 +0100

    server config and middlewares

---
+++ index.js
@@ -0,0 +1,64 @@
+const express=require('express') 
+const app=express();
+
+// parse incoming data
+const multer=require("multer");
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(multer().none());
+
+const flash=require('connect-flash');
+app.use(flash());
+
+// session config
+const session=require('express-session');
+app.use(session({
+    name:"holako",
+    secret:'esgen',
+    cookie: {expires:60*60*24*1000},
+    resave:false,
+    saveUninitialized:false
+}));
+
+// use passport
+const {passport}=require('./passport/index');
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// template engine base ejs
+app.set('view engine','ejs')
+
+const {authRouteHandler,unauthRouteHandler}=require('./router/authorization/index.js');
+// middleware
+const unAuthPath=['login',"auth",'register',"signup","js/\.*","css/\.*"].join("|");
+const regx=new RegExp(`^(?!\/(${unAuthPath})$).*$`,"g");
+app.use(regx,authRouteHandler);
+
+
+// route handlers
+const {auth,direction}=require('./router/auth.js');
+// static routes
+app.use(express.static("./static"));
+//routes
+app.get('/',require('./router/index.js'));
+app.get('/about',require('./router/about.js'));
+app.get('/login',unauthRouteHandler,require('./router/login.js'))
+
+
+app.post('/auth' ,auth,direction);
+app.get('/logout',require('./router/logout.js'));
+app.get('/register',unauthRouteHandler,require('./router/register.js'));
+
+// post
+const {signupHandler}=require('./router/signup')
+app.post('/signup',signupHandler,(req,res)=>{ 
+    if(req.signup.status){
+        req.signup.login();
+    }else{
+        res.status(401).json({message:req.signup.errorMessage,status:req.signup.status})
+    }
+});
+//================================================================
+
+app.listen(3000);
