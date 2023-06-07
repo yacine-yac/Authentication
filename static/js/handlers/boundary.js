29af12453fc652270bd6a2323e484e505dea803b
yacine <lentente1958@gmail.com>
2023-06-07 10:13:23 +0100

    handle boundary class and error template

--- static/js/handlers/boundary.js
+++ static/js/handlers/boundary.js
@@ -1,10 +1,59 @@
-export function errorBoundary({message}){
-    const template=`<div id="bndr">
-            <h2>${message}</h2>
-    </div>`
-    document.querySelector('body').insertAdjacentHTML('afterend',template);
-}
 
-export function removeboundary(){
-    document.getElementById('bndr').remove();
+
+
+/**
+ * Draw error message template
+ */
+export class Boundary{
+    static object=null;
+    /**
+     * 
+     * @property {{message:string,status:boolean} | null } message error state & message object
+     * @property {HTMLElement | null} element node parent 
+     */
+    constructor(){
+        if(Boundary.object!=null){ return;}
+            this.message=null;
+            this.element=null;
+    }
+    /**
+     * 
+     * @param {{message:string,status:boolean} | null } message error state & message object
+     */
+    setMessage(message){
+        this.message=message;
+    }
+    /**
+     * 
+     * @param {HTMLElement | null} element node parent 
+     */
+    setElement(element){
+            this.element=element;
+    }
+    /**
+     * draw pattern in node element
+     */
+    draw(){
+            const template=`<div id="bndr">
+                        <h2>${this.message.message}</h2>
+                </div>`;
+            document.querySelector('body').insertAdjacentHTML('afterend',template);
+            this.setElement(document.getElementById('bndr'));
+    }
+    /**
+     * remove pattern and init object state
+     */
+    removeboundary(){
+            this.element.remove();
+            this.setMessage(null);
+            this.setElement(null);
+    }
+    /**
+     * check if boundary is intiated
+     * @returns {Boundary} boundary object
+     */
+    static init(){ 
+            const obj= Boundary.object ?? new Boundary() ;
+            return obj;
+    }
 }
--- static/js/register.js
+++ static/js/register.js
@@ -1,12 +1,16 @@
 import fetching from "./handlers/ajax.js";
-import { errorBoundary, removeboundary } from "./handlers/boundary.js";
+import { Boundary } from "./handlers/boundary.js";
 import location from "./handlers/location.js";
 const button=document.getElementById('rs-1');
+const boundary=Boundary.init();
 button.addEventListener('click',function(e){
     e.preventDefault();
     const formdata=new FormData(document.querySelector('form'))
     fetching({method:'POST',url:'/signup'},formdata,location,(response)=>{
-         errorBoundary(response);
-         setTimeout(removeboundary,3500);
+        boundary.message ==null &&  (
+                    boundary.setMessage(response),
+                    boundary.draw(),
+                    setTimeout(function(){boundary.removeboundary()},3500)
+        );
     });
 });
