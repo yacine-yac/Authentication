8d5db4c956313b4a0443d29f48db7320bacd0596
yacine <lentente1958@gmail.com>
2023-06-08 10:05:40 +0100

    add filters to checkInputs method

--- model/signup.js
+++ model/signup.js
@@ -1,3 +1,5 @@
+
+
 /**
  * Handle user signup
  */
@@ -18,35 +20,37 @@ class Signup{
     }
     checkInputs(){
         const {name,sex,birth,email,password}=this.req.body;
-        if(!name){this.status=true; next();} 
-        if(!sex){this.status=true; next();} 
-        if(!birth){this.status=true; next();}
-        if(!email){this.status=true; next();}
-        if(!password){this.status=true; next();}
+        const {isEmail,isDate,isAlpha,isAlphanumeric}=require('validator');
+        console.log(birth,isDate(birth,{strictMode:true}));
+        if(!name || !isAlpha(name)){this.status=false;this.errorMessage="name not exists"; this.next();} 
+        if(!sex || !/^(Famale|Male)$/g.test(sex)){this.status=false;this.errorMessage="sex not exists"; this.next();} 
+        if(!birth || !isDate(birth,{delimiters:['.','/', '-']})){this.status=false;this.errorMessage="birth not exists"; this.next();}
+        if(!email || !isEmail(email)){this.status=false;this.errorMessage="email not exists"; this.next();}
+        if(!password || !isAlphanumeric(password)){this.status=false;this.errorMessage="password not exists"; this.next();}
 
         this.setUser();
     }
     setUser(){
-        const User=require('./user');
-        this.user=new User(...this.req.body);
+        const User=require('./user'),params=Object.values(this.req.body)
+        this.user=new User(...params);
         this.user.setId(this.generateId());
     }
     addUser(){
-            if(this.user.exists()){
-                this.status=true;
+            if(this.user?.exists()){
+                this.status=false;
                 this.errorMessage="User already exists !";
                 this.next();
             }
-            const insert=this.user.add();
+            const insert=this.user?.add();
             insert 
                     ? (this.status=true ) 
                     : (this.errorMessage="Insert user error",this.status=false,this.next());
     }
     login(){
         const session={id:this.user.id,name:this.user.name};
-        req.logIn(session,(err)=>{
-            if(err){ this.status=false; }
-            
+        this.req.logIn(session,(err)=>{
+            if(err){ this.status=false;}
+            this.res.json({status:this.status,messge:"succes, creation"})
         });
     }
     generateId(){
