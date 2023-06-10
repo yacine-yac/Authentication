bb93b15c3e12c73351251b95437106b59a671003
yacine <lentente1958@gmail.com>
2023-06-10 23:27:15 +0100

    add name params to each validation method

--- model/filtering/index.js
+++ model/filtering/index.js
@@ -5,34 +5,34 @@
 class Validation{
     constructor(){ 
             this.filters=require('validator');
-            this.error=[];
+            this.error={};
     }
     /**
      * Check if input is date form 
      * @param {*} field 
      * @returns {Boolean}
      */
-    date(field,{message}){
-        if(!this.filters.isDate(field,{delimiters:['.','/', '-']})){this.error.push(message)};
+    date(name,field,{message}){
+        if(!this.filters.isDate(field,{delimiters:['.','/', '-']})){this.error[name]=message};
     }
-    email(field,{message}){console.log('fffff',field);
-        if(!this.filters.isEmail(field)) this.error.push(message);
+    email(name,field,{message}){console.log('fffff',field);
+        if(!this.filters.isEmail(field)) this.error[name]=message;
     }
-    alphabets(field,{message}){
-      if(!this.filters.isAlpha(field))this.error.push(message);
+    alphabets(name,field,{message}){
+      if(!this.filters.isAlpha(field))this.error[name]=message;
     }
-    alphaNumeric(field,{message}){
-        if(!this.filters.isAlphanumeric(field)) this.error.push(message); 
+    alphaNumeric(name,field,{message}){
+        if(!this.filters.isAlphanumeric(field)) this.error[name]=message; 
     }
     /**
      * check if input has a special characters
      * @param {*} field 
      * @param {Array} characters characters that should be checked
      */
-    match(field,characters,{message}){
+    match(name,field,characters,{message}){
         const expressions=characters.join('|');
         const regx= new RegExp(`^(${expressions})$`,'g')
-        if(!regx.test(field)) this.error.push(message);
+        if(!regx.test(field)) this.error[name]=message;
     }
 }
 module.exports={Validation};
