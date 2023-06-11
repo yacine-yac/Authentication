0e22f4fa434e4317ad2810f99fb8b9bc23858f98
yacine <lentente1958@gmail.com>
2023-06-11 10:39:43 +0100

    define error object for login engine template

--- router/index.js
+++ router/index.js
@@ -1,9 +1,14 @@
 module.exports=(req,res)=>{
+    const {email,password,global}=req.flash('login')[0] ?? {};
     res.render('../views/index.ejs',{
         title:"Welcom",
         templatePath:"../templates/tmpl/login.ejs",
         path:`/auth${ req.query.redirect ? '?redirect='+req.query.redirect : ''}`,
-        message:null,
+        error:{
+            message:global,
+            email,
+            password
+        },
         script:'js/index.js'
     });
 }
--- router/login.js
+++ router/login.js
@@ -1,9 +1,14 @@
 module.exports=(req,res)=>{
+    const {email,password,global}=req.flash('login')[0] ?? {};
     res.render('../views/index.ejs',{
         title:"Login",
         templatePath:"../templates/tmpl/login.ejs",
         path:`/auth${ req.query.redirect ? '?redirect='+req.query.redirect : ''}`,
         script:'js/index.js',
-        message:req.flash('error')
+        error:{
+            message:global,
+            email,
+            password
+        }
     });
 }
