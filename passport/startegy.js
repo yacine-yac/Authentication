d7c8f21f3dac8ca224a7491e4ac4ec3d96b03f1a
yacine <lentente1958@gmail.com>
2023-06-11 16:23:08 +0100

    check auth steps

--- passport/startegy.js
+++ passport/startegy.js
@@ -5,9 +5,30 @@ const auth=new LocalPassport({usernameField:"email",passwordField:"password",pas
 function checking(email,password,callback){
     /** callback accepts two params 
      * 1=> error handling
-     * 2=> user (it can be user object or false)
+     * 2=> user (it can be user object or null)
      * 3=> message object 
      */
-    return callback(null,{name:"yacine",id:888})
+    const {UserBuilder}=require('../model/user')
+    const {AuthenticationController}=require('../controller/auth');
+    const controller=AuthenticationController.main;
+    const {email:e_email,password:e_password,global}=controller.error;
+    if(e_email  || e_password || global){
+        return callback(null,false,controller.error);
+    }
+    const userbuilder=new UserBuilder(undefined,undefined,undefined,email,password);
+    const user =userbuilder.setEmail(email).setPassword(password).build();
+
+
+    if(!user.identify()){
+        controller.error.global="User not exist"
+        return callback(null,false,controller.error)
+    }
+
+    if(!user.checkPassword()){
+        controller.error.global="Password not correct, try again"
+        return callback(null,false,controller.error)
+    }
+
+    return callback(null,user.session)
 }
 passport.use("local",auth); 
