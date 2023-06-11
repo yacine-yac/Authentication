b573c26bd812007383093c81de97c99b7bb4781d
yacine <lentente1958@gmail.com>
2023-06-11 10:36:54 +0100

    make autcontroller class in singleton design pattern

--- controller/auth.js
+++ controller/auth.js
@@ -1,30 +1,78 @@
+
+/**
+ * Handle credentials validation (signleton design pattern)
+ */
 class AuthenticationController{
-    constructor(req){
+    static main=null;
+    /**
+     * 
+     * @param {Request} req 
+     * @param {AuthError} error 
+     * @returns 
+     */
+    constructor(req,error){
+        if(AuthenticationController.main !=null) { return; }
         this.req=req;
         this.method=req.method;
         this.email=null;
         this.password=null;
         this.redirect=req.query.redirect ?? null;
-        this.error=[];
+        this.error=error;
     }
+    /**
+     * set credentials (email & password) based request method
+     */
     setCredentials(){
         const data=this.method =='POST' ? this.req.body : this.req.query;
         this.email=data?.email;
         this.password=data?.password;
     }
+    /**
+     * Check if credentials are not empty
+     * @returns {Boolean}
+     */
     check(){
-            if(!this.email){ this.error.push({email:"Please, provide an email"});}
-            if(!this.password){this.error.push({password:"Please, provide a password"});}
-           return  this.error.length>0 ? false : true;
+            if(!this.email){ this.error.email="Please, provide an email"}
+            if(!this.password){this.error.password="Please, provide a password";}
+           return  (this.error.email || this.error.password) ? false : true;
     }
+    /**
+     * valide external inputs (credentials & url variables)
+     * @param {Validation} validation 
+     * @returns {Boolean} 
+     */
     validation(validation){
-        if(this.error.length>0){ return;}
 
-        validation.email(this.email,{message:{email:"Please, provide a valide email"}});
-        validation.alphaNumeric(this.password,{message:{password:"Please, provide a valide password"}});
-        this.redirect && validation.alphabets(this.redirect,{message:"Bad request, check your Url"});
-        this.error= validation.error;
-        return this.error.length>0 ? false : true;
+      if(!this.error.email){
+            validation.email(this.email,{message:{email:"Please, provide a valide email"}});
+            this.error.email= validation.error.email;
+        }
+
+      if(!this.error.password){
+            validation.alphaNumeric(this.password,{message:{password:"Please, provide a valide password"}});
+            this.error.password= validation.error.password;
+        }
+      this.redirect && validation.alphabets(this.redirect,{redirect:"Bad request, check your Url"});
+
+        return (this.error.email || this.error.password) ? false : true;
+    }
+    /**
+     * initialize AuthenticationController object
+     * @param {Request} req 
+     * @param {AuthError} error 
+     * @returns {AuthenticationController}
+     */
+    static init(req,error){
+        if(AuthenticationController.main ==null){
+            AuthenticationController.main=new AuthenticationController(req,error); 
+        }
+        return AuthenticationController.main;
+    }
+    /**
+     * destroy AuthenticationController object
+     */
+    static destroy(){
+        AuthenticationController.main=null;
     }
 }
 module.exports={AuthenticationController};
