520357584aed4627c46fae6d68fb4d9265c4fe6d
yacine <lentente1958@gmail.com>
2023-06-11 16:07:20 +0100

    define userbuilder class for user class

--- model/user.js
+++ model/user.js
@@ -22,4 +22,43 @@ class User{
         this.id=id;
     }
 }
-module.exports=User;
+
+/**
+ * Used for build User Object
+ */
+class UserBuilder{
+    constructor(){
+        this.id=null;
+        this.name=null;
+        this.sex=null;
+        this.birth=null;
+        this.email=null;
+        this.password=null;
+    }
+    setName(name){
+            this.name=name;
+            return this;
+    }
+    setBirth(birth){
+        this.birth=birth;
+        return this;
+    }
+    setSex(sex){
+        this.sex=sex;
+        return this;
+    }
+    setEmail(email){
+        this.email=email;
+        return this;
+    }
+    setPassword(password){
+        this.password=password;
+        return this;
+    }
+    build(){
+        return new User(this.name,this.sex,this.birth,this.email,this.password);
+    }
+}
+
+
+module.exports={User,UserBuilder};
