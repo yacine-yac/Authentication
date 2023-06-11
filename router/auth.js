b2106c2634a60dad9e9ca93d8d471a2c03ddc2ba
yacine <lentente1958@gmail.com>
2023-06-11 20:16:00 +0100

    auth passport callback

--- router/auth.js
+++ router/auth.js
@@ -1,33 +1,38 @@
+const {AuthenticationController}=require('../controller/auth');
 async function auth(req,res,next){
     const {passport}=require('../passport/index.js');
     const {Authentication}=require('../model/auth.js');
 
-console.log(authController.redirect)
-    const auth=new Authentication(authController);
-    auth.setbadRequestMessage();
-    auth.setfailureRedirect();
-      passport.authenticate('local', { 
-                  failureRedirect:auth.redirection, 
-                  failureFlash:true,
-                  badRequestMessage:auth.message,
-                  failureMessage:true,
-      })(req,res,next);
+    const authModel=new Authentication(AuthenticationController.main);
+    
+    req.flash('login',authModel.auth.error);
+
+           passport.authenticate('local', {
+                  // failureRedirect:authModel.redirection,
+                  failWithError:true,
+                  // failureFlash:true,
+                  // failureMessage:true
+                  // successRedirect:"/about"
+          },(defError,user,info)=>{
+             const error=authModel.auth.error;
+                 if(error.email || error.password || error.global) 
+                         authModel.redirect();
+                 else
+                          authModel.login(user);
+          })(req,res,next);
+     AuthenticationController.destroy();
+
 
 }
-function direction(req,res){
-  console.log(req.session)
-  const direction =req.query.redirect ?? "/";
-  res.redirect(direction);
-}
 function authController(req,res,next){
-  const {Validation}=require('../model/filtering/index.js');
-  const {AuthenticationController}= require('../controller/auth.js');
-  const authController=new AuthenticationController(req);
+  const {Validation}=require('../model/filtering/index.js'); 
+  const {AuthError}=require('../model/authError');
+  const authController= AuthenticationController.init(req,new AuthError());
   authController.setCredentials(); 
-  authController.check();
+  authController.check(); 
   authController.validation(new Validation());
   next();
 }
-module.exports={direction,auth,authController} 
+module.exports={auth,authController} 
    
  
