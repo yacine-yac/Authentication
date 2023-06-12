6df4a234d64fe6481ce1561e6eafa570624271d9
yacine <lentente1958@gmail.com>
2023-06-12 08:58:57 +0100

    solve validation problem

--- controller/signup/index.js
+++ controller/signup/index.js
@@ -18,13 +18,13 @@ class SignupController{
      */
     check(){
         const {name,birth,email,password}=this.data;
-        const errors=[];
-        if(!name) errors.push('Please fill in a name');
-        if(!birth) errors.push('Please fill in a complete birthday');
-        if(!email) errors.push('Please fill in an address email');
-        if(!password) errors.push('Enter a password ');
+        const errors=this.state.errorMessage;
+        if(!name) this.state.setName('Please fill in a name');
+        if(!birth) this.state.setBirth('Please fill in a complete birthday');
+        if(!email) this.state.setEmail('Please fill in an address email');
+        if(!password) this.state.setPassword('Enter a password');
 
-        if(errors.length>0){
+        if(Object.values(errors).length>0){
             this.state.status=false;
             this.state.errorMessage=errors;
         }
@@ -34,14 +34,14 @@ class SignupController{
      */
     validation(){ 
         const {name,sex,birth,email,password}=this.data;
-        if(!this.state.status) return ;
+          if(!this.state.status) return ;
 
-        this.valide.alphabets(name,{message:"Please enter a valid name (with only alphabets)"});
-        this.valide.email(email,{message:'Please enter a valid  email'});
-        this.valide.date(birth,{message:'Please enter a valid date '});
-        this.valide.match(sex,["Famale","Male"],{message:"Please select your gender"});
-        this.valide.alphaNumeric(password,{message:"Please enter a valid password (with alphabets,numeric and more then 8 charcters"});
-        this.valide.error.length > 0 
+        this.valide.alphabets(name,{message:{name:"Please enter a valid name (with only alphabets)"}});
+        this.valide.email(email,{message:{email:'Please enter a valid  email'}});
+        this.valide.date(birth,{message:{birth:'Please enter a valid date '}});
+        this.valide.match(sex,["Famale","Male"],{message:{sex:"Please select your gender"}});
+        this.valide.alphaNumeric(password,{message:{password:"Please enter a valid password (with alphabets,numeric and more then 8 charcters"}});
+        Object.values(this.valide.error).length > 0 
                     ? (
                         this.state.status=false,
                         this.state.errorMessage=this.valide.error
